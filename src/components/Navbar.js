8e0d380764319b4bca704bf3874dd6999d546b52
Daffasurya <daffasuryaarrayan@gmail.com>
2023-02-12 15:03:39 +0700

    Login responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,39 @@
 import "./Navbar.css";
 import logo from "../assets/logo.png"
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import { useEffect, useState } from "react";
+import Login from "../pages/Login";
 
 
 const Navbar = () => {
+    const [login, setlogin] = useState(false);
+    const [isemail, setemail] = useState('');
+    const navigate = useNavigate('');
+    
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        const Email = localStorage.getItem('email')
+
+    if(!token) {
+        setlogin(false)
+    } else {
+        setlogin(true)
+    }
+
+    if(Email) {
+        setemail(Email)
+    } else {
+        setemail()
+    }
+})
+
+  const Handleremove = () => {
+    localStorage.removeItem('token')
+    localStorage.removeItem('email')
+    navigate('/');
+  }
     return (
-        
-            <div className="navbar">
+        <div className="navbar">
                 <div className="logo"><Link to="/"><img src={logo}/></Link></div>
                  <div className="nav-list">
                      <ul>
@@ -14,10 +41,21 @@ const Navbar = () => {
                          <li><a href="#whyus">Why Us</a></li>
                          <li><a href="#testimonial">Testimonial</a></li>
                          <li><a href="#faq">FAQ</a></li>
+                         {login ?
+                         <button className="button-navbar" onClick={Handleremove}>Logout</button>
+                          : 
                          <Link to={"/Register"}>
-                        <button className="button-navbar">Register</button>
-                        </Link>
+                         <button className="button-navbar">Register</button>
+                         </Link>}
+                         {login ?  <div className="profile-navbar">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class="bi bi-person-circle" viewBox="0 0 16 16">
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                     <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                    </svg>
+                    <p className="p-profile">{isemail}</p>
+                     </div> : null}
                      </ul>
+                   
                  </div>
             </div>
         
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,81 @@
+import { Link, useNavigate } from "react-router-dom";
+import "../pages/Login.css";
+import Landingpage from "../assets/Landing page - Desktop.png"
+import { useState } from "react";
+import axios from "axios";
 const Login = () => {
+    const [Myemail, setMyemail] = useState('');
+    const [pas, setpas] = useState('');
+    const [Error, setError] = useState('');
+    const navigate = useNavigate('');
+
+    const Handleislogin = (e) => {
+      setMyemail(e.target.value)
+    }
+
+    const Handleispas = (e) => {
+        setpas(e.target.value)
+    }
+
+    const HandlesignIn = async() => {
+        if(!Myemail && !pas) {
+          setError('Masukkan email dan password terlebih dahulu')  
+        } else {
+            const payload = {
+                email: Myemail,
+                password: pas,
+            }
+
+          try {
+            const res = await axios.post(`https://bootcamp-rent-cars.herokuapp.com/customer/auth/login`, payload)
+            console.log(res)
+            localStorage.setItem('token' , res.data.access_token)
+            localStorage.setItem('email', res.data.email)
+            alert('Welcome Customer Binar...')
+            navigate('/Carimobil')
+          } catch (error){
+                console.log(error.response)
+                setError(error.response.data.message)
+          }
+        }
+
+        console.log(Error);
+    }
+
+    console.log(Myemail)
+    console.log(pas)
     return (
         <div>
-            <h2>ini halaman Login</h2>
+            <div className="div-content">
+                <div className="div-loginAuth">
+                  <p className="p-graylogo"></p>
+                  <p className="p-titlelogin">Welcome Back</p>
+                  <p className="p-Emaillogin">Email</p>
+                  <input type={"text"} className="input-emaillogin" placeholder="Masukkan Email" onChange={Handleislogin}/>
+                  <p className="p-passwordlogin">Password</p>
+                  <input type={"password"} className="input-paslogin" placeholder="Masukkan password" onChange={Handleispas}/>
+                  <div className="div-button-login">
+                  <button className="button-signin" onClick={HandlesignIn}>Sign In</button>
+                  </div>
+                  <div className="div-informationAuth">
+                     <p className="p-informationAuth">Don’t have an account?</p>
+                     <Link to={'/Register'} className="link-auth">
+                     <p className="p-directsignUp">Sign Up</p>
+                     </Link>
+                  </div>
+                  {Error ? <div className="div-handleerror">
+                   <p>{Error}</p>
+                  </div>
+                  : null}
+                </div>
+                
+                {/* bates-bates */}
+
+                <div className="div-content2">
+                    <p className="p-Auth-login">Binar Car Rental</p>
+                    <img src={Landingpage} className="img-Auth"/>
+                </div>
+            </div>
         </div>
     );
 }
