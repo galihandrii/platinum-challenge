b4e0a589d837006d5a9074ca244e40604e78d20e
Your Name <you@example.com>
2023-02-14 21:26:27 +0400

    adding new file dropzone and adding dropzone in paymentcomplete

---
+++ src/components/DropZone.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import {useDropzone} from 'react-dropzone';
+
+function DropZone() {
+  const [files, setFiles] = useState([]);
+
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({
+    accept: 'image/*',
+    onDrop: acceptedFiles => {
+      setFiles(acceptedFiles.map(file => Object.assign(file, {
+        preview: URL.createObjectURL(file)
+      })));
+    }
+  });
+
+  const style = {
+    borderColor: '#6c757d',
+    borderStyle: 'dashed',
+    backgroundColor: '#fafafa',
+    borderWidth: 2,
+    borderRadius: 5,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 100,
+    transition: 'border .2s ease-in-out'
+  };
+
+  return (
+    <div {...getRootProps()} className="dropzone" style={style}>
+      <input {...getInputProps()} />
+      {isDragActive ?
+        <p>Drop the files here ...</p> :
+        <p>Drag and drop your files here, or click to select files</p>
+      }
+      {files.map(file => (
+        <img
+          key={file.name}
+          src={file.preview}
+          alt={file.name}
+          style={{height: 100, margin: 10}}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default DropZone;
--- src/pages/PaymentCompleted.js
+++ src/pages/PaymentCompleted.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { useDropzone } from 'react-dropzone';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
+import DropZone from '../components/DropZone';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Rectangle36 from '../assets/Rectangle_36.jpg';
@@ -14,6 +14,8 @@ import './Paymentcompleted.css'
 import { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import Countdown from 'react-countdown';
+import { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
 import './Paymentf.css'
 
 const PaymentCompleted = (props) => {
@@ -23,10 +25,14 @@ const PaymentCompleted = (props) => {
     
     const [confirm, setConfirm] = useState(false);
     const navigate = useNavigate()
+    
 
     const handleConfirm = () => {
         setConfirm(true);
     }
+
+    
+    
 
     const Completionist = () => {
         return ( <span>You are good to go!</span> )
@@ -119,93 +125,99 @@ const PaymentCompleted = (props) => {
                     </div>
 
                     <div className='main-content'>
-                            <div className='content-left'>
-
-                                <div className='card mb-2'>
-                                    <div className='kelas-pembayaran'>
-                                        <div className='bagian1-kelas-pembayaran'>
-                                            <p className='judul'>Lakukan Pembayaran Sebelum</p>
-                                            <p className='judul-1'>Rabu, 19 Mei 2022 jam 13.00 WIB</p>
-                                        </div>
-                                        <div className='bagian2-kelas-pembayaran'>
-
-                                        {/* react-countdown-below */}
-                                        <Countdown
-                                            date={Date.now() - 5000}
-                                            renderer={renderer}
-                                        />,
-
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className='card mb-2'>
-                                    <div className='lakukan-transfer'>
-                                        <p className='judul-lakukan-transfer'>Lakukan Transfer Ke</p>
-
-                                        <div className='lakukan-transfer-bank'>
-                                            <div className='opsi-1'>
-                                                <p className='font'>BCA</p>
+                        
+                            
+                                <div className='content-left'>
+                                
+                                    <div className='card mb-2'>
+                                        <div className='kelas-pembayaran'>
+                                            <div className='bagian1-kelas-pembayaran'>
+                                                <p className='judul'>Lakukan Pembayaran Sebelum</p>
+                                                <p className='judul-1'>Rabu, 19 Mei 2022 jam 13.00 WIB</p>
                                             </div>
-                                            <div className='opsi-2'>
-                                                <p className='font-1'>BCA Transfer</p>
-                                                <p className='font-1'>a.n Binar Car Rental</p>
-                                            </div>
-                                        </div>
+                                            <div className='bagian2-kelas-pembayaran'>
 
-                                        <div className='copas-thecode'>
-                                            <p className='title'>Nomor Rekeninng</p>
-                                            <div className='copy-thecode'>
-                                                <p className='detail-thecode'>14521</p>
-                                                <img src={Copy} />
-                                            </div>
+                                            {/* react-countdown-below */}
+                                            <Countdown
+                                                date={Date.now() - 5000}
+                                                renderer={renderer}
+                                            />,
 
-                                            <p className='title'>Total Bayar</p>
-                                            <div className='copy-thecode'>
-                                                <p className='detail-thecode'>14521</p>
-                                                <img src={Copy} />
                                             </div>
                                         </div>
                                     </div>
-                                </div>
 
-                                <div className='card mb-2'>
-                                    <div className='instruksi-pembayaran'>
-                                        <h5 className='upside'>Instruksi Pembayaran</h5>
-                                        <div className='cara2pembayaran'>
-                                            <p className='cara-1'>ATM BCA</p>
-                                            <p className='cara-2'>M-BCA</p>
-                                            <p className='cara-2'>BCA Klik</p>
-                                            <p className='cara-2'>Internet Banking</p>
+                                    <div className='card mb-2'>
+                                        <div className='lakukan-transfer'>
+                                            <p className='judul-lakukan-transfer'>Lakukan Transfer Ke</p>
+
+                                            <div className='lakukan-transfer-bank'>
+                                                <div className='opsi-1'>
+                                                    <p className='font'>BCA</p>
+                                                </div>
+                                                <div className='opsi-2'>
+                                                    <p className='font-1'>BCA Transfer</p>
+                                                    <p className='font-1'>a.n Binar Car Rental</p>
+                                                </div>
+                                            </div>
+
+                                            <div className='copas-thecode'>
+                                                <p className='title'>Nomor Rekeninng</p>
+                                                <div className='copy-thecode'>
+                                                    <p className='detail-thecode'>14521</p>
+                                                    <img src={Copy} />
+                                                </div>
+
+                                                <p className='title'>Total Bayar</p>
+                                                <div className='copy-thecode'>
+                                                    <p className='detail-thecode'>14521</p>
+                                                    <img src={Copy} />
+                                                </div>
+                                            </div>
                                         </div>
-                                        <p className='langkahpembayaran'>Masukan kartu ATM, lalu PIN</p>
-                                        <p className='langkahpembayaran'>Pilih menu kemudian transfer </p><p className='langkahpembayaran'>Lakukan saja apa yang anda lakukan</p><p className='langkahpembayaran'>Sekarepmu dewe</p>
+                                    </div>
+
+                                    <div className='card mb-2'>
+                                        <div className='instruksi-pembayaran'>
+                                            <h5 className='upside'>Instruksi Pembayaran</h5>
+                                            <div className='cara2pembayaran'>
+                                                <p className='cara-1'>ATM BCA</p>
+                                                <p className='cara-2'>M-BCA</p>
+                                                <p className='cara-2'>BCA Klik</p>
+                                                <p className='cara-2'>Internet Banking</p>
+                                            </div>
+                                            <p className='langkahpembayaran'>Masukan kartu ATM, lalu PIN</p>
+                                            <p className='langkahpembayaran'>Pilih menu kemudian transfer </p><p className='langkahpembayaran'>Lakukan saja apa yang anda lakukan</p><p className='langkahpembayaran'>Sekarepmu dewe</p>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div className='content-right'>
+
+                                    <div className='card mb-5'>
+                                        <div className='kelas-pembayarankanan'>
+                                            <p className='judul-kanan'>Klik konfirmasi pembayaran untuk mempercepat proses pengecekan</p>
+
+                                            {
+                                                confirm ? (
+                                                    <>
+                                                        <p className='judul-kanan'>Terima kasih telah melakukan konfirmasi pembayaran. Pembayaranmu akan segera kami cek tunggu kurang lebih 10 menit untuk mendapatkan konfirmasi.</p>
+                                                        <p className='judul-kanan'>Upload Bukti Pembayaran</p>
+                                                        <p className='judul-kanan'>Untuk membantu kami lebih cepat melakukan pengecekan. Kamu bisa upload bukti bayarmu</p>
+
+                                                        <DropZone />
+                                                        
+                                                        <button className='btn btn-success w-100' onClick={uploadFile}>Upload</button>
+                                                    </>
+                                                ) : <button className='btn btn-success w-100 tombol-kanan' onClick={handleConfirm}>Konfirmasi Pembayaran</button>
+                                            }
+
+                                            
+                                        </div>
                                     </div>
                                 </div>
-
-                            </div>
-                            <div className='content-right'>
-
-                                <div className='card mb-5'>
-                                    <div className='kelas-pembayarankanan'>
-                                        <p className='judul-kanan'>Klik konfirmasi pembayaran untuk mempercepat proses pengecekan</p>
-
-                                        {
-                                            confirm ? (
-                                                <>
-                                                    <p className='judul-kanan'>Terima kasih telah melakukan konfirmasi pembayaran. Pembayaranmu akan segera kami cek tunggu kurang lebih 10 menit untuk mendapatkan konfirmasi.</p>
-                                                    <p className='judul-kanan'>Upload Bukti Pembayaran</p>
-                                                    <p className='judul-kanan'>Untuk membantu kami lebih cepat melakukan pengecekan. Kamu bisa upload bukti bayarmu</p>
-                                                    <input onChange={handleImage} className='masukan-buktitf' type='file' />
-                                                    <button className='btn btn-success w-100' onClick={uploadFile}>Upload</button>
-                                                </>
-                                            ) : <button className='btn btn-success w-100 tombol-kanan' onClick={handleConfirm}>Konfirmasi Pembayaran</button>
-                                        }
-
-                                        
-                                    </div>
-                                </div>
-                        </div>
+                            
+                        
                     </div>
 
             <Footer />
